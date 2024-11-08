5d60260eeb7a75b947fb102269fbca2480a611d1
cainiao <wdhlzd@163.com>
2024-11-08 17:39:56 +0800

    分页查询流程实例
    Signed-off-by: cainiao <wdhlzd@163.com>

--- src/main/java/org/cainiao/process/service/impl/ProcessDefinitionMetadataServiceImpl.java
+++ src/main/java/org/cainiao/process/service/impl/ProcessDefinitionMetadataServiceImpl.java
@@ -1,12 +1,22 @@
 package org.cainiao.process.service.impl;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
 import org.cainiao.process.dao.service.ProcessDefinitionMetadataMapperService;
 import org.cainiao.process.dto.response.ProcessInstanceResponse;
 import org.cainiao.process.entity.ProcessDefinitionMetadata;
 import org.cainiao.process.service.ProcessDefinitionMetadataService;
+import org.flowable.engine.HistoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.history.HistoricProcessInstanceQuery;
+import org.flowable.engine.runtime.Execution;
+import org.flowable.engine.runtime.ProcessInstanceQuery;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <br />
@@ -18,6 +28,8 @@ import org.springframework.stereotype.Service;
 public class ProcessDefinitionMetadataServiceImpl implements ProcessDefinitionMetadataService {
 
     private final ProcessDefinitionMetadataMapperService processDefinitionMetadataMapperService;
+    private final HistoryService historyService;
+    private final RuntimeService runtimeService;
 
     @Override
     public IPage<ProcessDefinitionMetadata> processDefinitions(long systemId,
@@ -33,7 +45,40 @@ public class ProcessDefinitionMetadataServiceImpl implements ProcessDefinitionMe
     @Override
     public IPage<ProcessInstanceResponse> processInstances(long systemId, String processDefinitionKey,
                                                            Boolean finished, long current, long size) {
-        // TODO
-        return null;
+        HistoricProcessInstanceQuery historicProcessInstanceQuery = historyService
+            .createHistoricProcessInstanceQuery().processDefinitionKey(processDefinitionKey);
+        if (finished != null) {
+            if (finished) {
+                historicProcessInstanceQuery.finished();
+            } else {
+                historicProcessInstanceQuery.unfinished();
+            }
+        }
+
+        int count = (int) historicProcessInstanceQuery.count();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        ProcessInstanceQuery processInstanceQuery = runtimeService.createProcessInstanceQuery();
+        IPage<ProcessInstanceResponse> page = new Page<>(current, size);
+        page.setRecords(historicProcessInstanceQuery.orderByProcessInstanceStartTime().desc()
+            .listPage((int) ((current - 1) * size), (int) size)
+            .stream().map(historicProcessInstance -> {
+                ProcessInstanceResponse processInstanceResponse = ProcessInstanceResponse
+                    .from(historicProcessInstance, simpleDateFormat);
+                if (!processInstanceResponse.isEnded()) {
+                    // 未结束的流程，查询一下：哪些节点正在执行、是否处于暂停状态
+                    List<Execution> executions = runtimeService.createExecutionQuery()
+                        .processInstanceId(historicProcessInstance.getId())
+                        .list();
+                    if (executions != null && !executions.isEmpty()) {
+                        processInstanceResponse.setActivityIds(executions.stream()
+                            .map(Execution::getActivityId).filter(Objects::nonNull).toList());
+                    }
+                    processInstanceResponse.setSuspended(processInstanceQuery
+                        .processInstanceId(historicProcessInstance.getId()).singleResult().isSuspended());
+                }
+                return processInstanceResponse;
+            }).toList());
+        page.setTotal(count);
+        return page;
     }
 }
